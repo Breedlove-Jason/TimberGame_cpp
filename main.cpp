4be6e3c2b2e79b8e361a70c641f1f59fe31d2823
breedlove-jason <j@jasonbreedlove.com>
2024-04-21 14:36:21 -0700

    add clouds, bee, and tree to screen

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+#include <cmath>
+#include <cstdio>
 
 #include "Timber.h"
 
@@ -10,6 +12,24 @@ namespace fs = std::filesystem;
 using namespace std;
 
 int main() {
+
+        // What is the player's score?
+        int playerScore = 0;
+
+        // What is the player's first initial?
+        char playerInitial = 'J';
+
+        //What is the value of pi?
+        const float valuePi = M_PI;
+
+        // Is the player alive or dead
+        bool isAlive = true;
+
+        // get the random number and assign it to a variable
+        int number = rand() % 100;
+
+
+        // check graphics module is working
         fs::path pathToImage = fs::current_path() / "graphics/background.png";
         std::cout << "Full path to image: " << pathToImage << std::endl;
 
@@ -24,7 +44,7 @@ int main() {
     VideoMode vm(1920, 1080);
 
     // create and open a window for the game
-    RenderWindow window(vm, "Timber!!!"); // set style to fullscreen when ready
+    RenderWindow window(vm, "Timber!!!"); // set style to fullscreen when ready Style::Fullscreen
 
     // create a texture to hold a graphic on the GPU
     Texture textureBackground;
@@ -42,6 +62,59 @@ int main() {
 
     // set the spriteBackground to cover the screen
     spriteBackground.setPosition(0, 0);
+
+    // make a tree sprite
+
+    Texture textureTree;
+    if (!textureTree.loadFromFile("graphics/tree.png")) {
+        cout << "Can't load tree" << endl;
+    }
+    else {
+        cout << "Tree loaded" << endl;
+
+        textureTree.loadFromFile("graphics/tree.png");
+    }
+    Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810, 0);
+
+    // prepare the bee
+    Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+    Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0, 800);
+
+    // Is the bee currently moving?
+    bool beeActive = false;
+
+    // How fast can the bee fly
+    float beeSpeed = 0.0f;
+
+    // prepare the clouds
+    Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+    Sprite spriteCloud1;
+    Sprite spriteCloud2;
+    Sprite spriteCloud3;
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+
+    // position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen// position clouds on the screen
+    spriteCloud1.setPosition(0, 0);
+    spriteCloud2.setPosition(0, 250);
+    spriteCloud3.setPosition(0, 500);
+
+    // are the clouds moving
+    bool cloud1Active = false;
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+
+    // how fast do they move
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3Speed = 0.0f;
 
     while (window.isOpen()) {
         /*
@@ -72,6 +145,13 @@ int main() {
 
         // draw game scene here
         window.draw(spriteBackground);
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+
+        window.draw(spriteTree);
+
+        window.draw(spriteBee);
 
         // show everything we just drew
         window.display();
